6d8fc1ca21bf6f6bbac4275bcd800207af58c158
JosephineAkua <joseadzoyiyayra@gmail.com>
2024-05-19 14:31:48 +0000

    Create userInfo.js file and add createUserProfiles function

---
+++ userInfo.js
@@ -0,0 +1,14 @@
+import { words, strings } from "./arrayManipulation.js";
+const createUserProfiles = (names, modifiedNames) => {
+    let id = 1;
+    return names.map((name, index) => {
+        return{
+            originalName: name, 
+            modifiedName: modifiedNames[index],
+            id: id++
+        };
+    });
+}
+
+const profiles = createUserProfiles(words, strings);
+console.log(profiles);
